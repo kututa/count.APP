56ec525429990bf865cc561f4825ec33d639a92a
kututa <vinnykututa@gmail.com>
2023-05-12 06:25:00 +0300

    count.App used for counting

---
+++ kali.js
@@ -0,0 +1,179 @@
+// console.log("hello vincent");
+
+// let name = 'vincent';
+// console.log(name);
+
+
+
+// let firstName = 'vincent'
+// lastName = 'kututa';
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate)
+
+// let name = 'vincent';
+// let age = 30;
+// let isApproved = false;
+// let firstName = undefined;
+// let selectedColor = null;
+
+// let person = {
+//    / name: 'vincent',
+// age: 30
+// };
+// 
+
+// person['name'] = 'wema';
+
+// let selection = 'name';
+// person[selection] = 'wema';
+// console.log(person.name);
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'green';
+// console.log(selectedColors);
+
+// Array
+
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+// function performing task
+
+// function greet(name) {
+// console.log('Hello ' + name);
+// }
+
+// greet('vincent kututa');
+// greet('pinna');
+
+
+// function performing calculating numbers
+
+// function paka(number) {
+// return number * number;
+// }
+
+
+// console.log(paka(7));
+
+// document.getElementById('count-el').innerText = 5
+
+// let myAge = 24
+// console.log(myAge)
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+// console.log(count);
+
+// let myAge = 24
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+
+// let MyAge = 24
+// let WemaMyAgeRatio = 12
+
+// let WemaAge = MyAge * WemaMyAgeRatio
+
+// console.log(WemaAge)
+
+
+function increment() {
+    console.log("the button was clicked")
+
+}
+
+
+
+function countdown() {
+
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+countdown()
+
+
+countdown()
+
+
+function mylog() {
+    console.log(42)
+}
+
+mylog()
+
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+
+function carRace() {
+    // let totalTime = lap1 + lap2 + lap3
+    // or
+    console.log(lap1 + lap2 + lap3)
+
+}
+
+carRace()
+
+// 
+
+
+
+
+// let countEl = document.getElementById("count-el")
+// console.log(countEl)
+
+// let count = 3
+
+// function increment() {
+// count = count + 1
+// countEl.innerText = count
+// console.log(count)
+// }
+
+let username = "per"
+
+
+
+let massage = "your have three notification"
+console.log(massage + "," + username)
+
+
+
+
+
+
+
+
+
+let vincent = document.getElementById("count-el")
+console.log(vincent)
+
+let count = 4
+
+function increment() {
+    count = count + 1
+    vincent.innerText = count
+
+}
+
+
+
+
+function save() {
+    console.log(count)
+
+}
